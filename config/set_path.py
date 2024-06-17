664740211643126236343e969351697cfb131523
Aditi Vijayan <aditi.vijayan@anu.edu.au>
2024-06-18 07:04:55 +1000

    add h5 path

--- config/set_path.py
+++ config/set_path.py
@@ -6,5 +6,6 @@ script_path   = os.path.join(home, 'scripts')
 notebook_path = os.path.join(home, 'notebook')
 lib_path      = os.path.join(home, 'lib')     
 fig_path      = os.path.join(home, 'figures')
+h5_path      = os.path.join(home, 'h5Data/')
 data_home         = os.path.join(qhome, 'quokka/sims/')
 scratch       = '/scratch/jh2/av5889/'
